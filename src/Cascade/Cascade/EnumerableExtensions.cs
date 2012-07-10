bcddd3f62ebf552165765fba5b192e8f1736e812
jbuechner <jbuechner@github>
2012-07-10 11:34:34 +0200

    - Added overload for GetValueOrDefault extension method to return an object value instead of a generic type (useful if the return value is not covariant to the target type) - Added Cast extension method for enumerations to cast an enumeration using a Converter delegate - Added HasFlag extension method which can be used to determine whether a flag value is set on an enum or an integral type

--- src/Cascade/Cascade/EnumerableExtensions.cs
+++ src/Cascade/Cascade/EnumerableExtensions.cs
@@ -33,5 +33,24 @@ namespace Cascade
                 foreach (T targetValue in targetValues)
                     callback(targetValue);
         }
+
+        /// <summary>
+        /// This method iterates over an enumeration and use for each item the <paramref name="castCallback"/> to return an enumeration of a different type.
+        /// </summary>
+        /// <typeparam name="TInput">The type parameter of the items in the input values enumeration.</typeparam>
+        /// <typeparam name="TOutput">The type parameter of the items returned by this method.</typeparam>
+        /// <param name="inputValues">An enumeration of items that should be casted.</param>
+        /// <param name="castCallback">A callback that is called for each item in <paramref name="inputValues"/>. The input value is passed as a parameter to the callback. The callback returns a casted or converted value.</param>
+        /// <returns>
+        /// Returns an enumeration of <typeparamref name="TOutput"/>. The enumeration is empty if either <paramref name="inputValues"/> is empty or <c>null</c>, or <paramref name="castCallback"/> is <c>null</c>.
+        /// </returns>
+        public static IEnumerable<TOutput> Cast<TInput, TOutput>(this IEnumerable<TInput> inputValues, Converter<TInput, TOutput> castCallback)
+        {
+            if (inputValues == null || castCallback == null)
+                yield break;
+
+            foreach (TInput inputValue in inputValues)
+                yield return castCallback(inputValue);
+        }
     }
 }
